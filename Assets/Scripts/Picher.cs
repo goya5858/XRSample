dae848452b6d46f4af6cae738350653c895a2234
goya5858 <hokuzaki1203@gmail.com>
2022-01-23 14:54:46 +0900

    AddSomeScripts

--- Assets/Scripts/Picher.cs
+++ Assets/Scripts/Picher.cs
@@ -6,31 +6,39 @@ public class Picher : MonoBehaviour
 {
     public GameObject ballPrefab;
     public Transform ballSpawnOffset;
-    float span = 3.0f;
-    float deltaTime = 0;
 
-    float projectionPower = 400f;
+    [SerializeField]
+    public float span = 3.0f;
+    public float deltaTime = 0;
+
+    [SerializeField]
+    public float projectionPower = 800f;
     float destroyTime = 3.0f;
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
+    [SerializeField]
+    public bool isThrow = false;
+
+    //void Start()
+    //{}
 
     // Update is called once per frame
     void Update()
-    {
-        this.deltaTime += Time.deltaTime;
-        if (this.deltaTime > this.span)
+    {   
+        if (isThrow)
         {
-            this.deltaTime = 0;
-            GameObject cloneBall = Instantiate(ballPrefab, ballSpawnOffset.position, ballSpawnOffset.rotation);
-            Rigidbody ballRigidbody = cloneBall.GetComponent<Rigidbody>();
-            if (ballRigidbody != null)
+            this.deltaTime += Time.deltaTime;
+            if (this.deltaTime > this.span)
             {
-                ballRigidbody.AddForce(cloneBall.transform.forward * projectionPower);
+                this.deltaTime = 0;
+                GameObject cloneBall = Instantiate(ballPrefab, ballSpawnOffset.position, ballSpawnOffset.rotation);
+                Rigidbody ballRigidbody = cloneBall.GetComponent<Rigidbody>();
+                if (ballRigidbody != null)
+                {
+                    ballRigidbody.AddForce(cloneBall.transform.forward * projectionPower);
+                }
+                Destroy(cloneBall, destroyTime);
             }
-            Destroy(cloneBall, destroyTime);
         }
     }
 }
